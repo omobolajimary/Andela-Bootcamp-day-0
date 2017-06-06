19c62d0a1f9da718f81c0b8a51538a523c541fb7
omobolajimary <omobolajimary@gmail.com>
2017-06-06 10:51:57 +0100

    passed test

--- spec/spec_primeTest.js
+++ spec/spec_primeTest.js
@@ -22,20 +22,9 @@ describe("Determine the sequence of an array of numbers: ", function(){
 
     it("should return invalid for an empty ", function(){
         expect(myApp.getPrime("")).toEqual("invalid type");
-    it("should return undefined for a negative numbers", function(){
-        expect(myApp.getPrime(-34)).toBe("undefined");
+    it("should return invalid for a float", function(){
+        expect(myApp.getPrime(34.6)).toBe("invalid type");
       });
       });
     });
-  });
-describe("Determine the sequence of an array of numbers: ", function(){
-    describe("Case for an empty array ", function(){
-
-    it("should return [2,3,5] for 5 ", function(){
-        expect(myApp.getPrime(5)).toEqual("[2,3,5]");
-    it("should return [2, 3, 5, 7,11,13] for 13", function(){
-        expect(myApp.getPrime(13)).toBe("[2, 3, 5, 7,11,13] ");
-      });
-      });
-    });
-  });
+  })
