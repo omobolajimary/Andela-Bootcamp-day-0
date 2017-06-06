c4530e308ec9355755ab82894c03aa165a7b2553
omobolajimary <omobolajimary@gmail.com>
2017-06-06 08:01:55 +0100

    test failed

---
+++ app/prime.js
@@ -0,0 +1,24 @@
+
+"use strict"
+module.exports={
+    
+getPrime:(n) =>{
+    var primes = [];
+    for(var i = 2; i < n; i++) {
+        primes[i] = true;
+    }
+    for(i = 2; i < n; i++) {
+        if(primes[i] === true) {
+            for(var j = i * i; j < n; j += i) {
+                primes[j] = false;
+            }
+        }
+    }
+    for(i = 2; i < n; i++) {
+        if(primes[i] === true) {
+          console.log(i)
+        }
+    }
+}
+}
+
